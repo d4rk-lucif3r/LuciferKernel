40a5888889ebe744968477a2675cd7335f6bc5ec
Sami Tolvanen <samitolvanen@google.com>
2018-05-02 10:44:59 -0700

    ANDROID: add support for clang Shadow Call Stack (SCS)
    This change adds generic support for clang's Shadow Call Stack, which
    uses a shadow stack to protect return addresses from being overwritten
    by an attacker. Details are available here:
    
      https://clang.llvm.org/docs/ShadowCallStack.html
    
    Bug: 112277034
    Change-Id: Idd553b7c978b0673ab533a68980fb9a654f4510c
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>
    
    Signed-off-by: d4rk-lucif3r <lucifer78908@gmail.com>
    Signed-off-by: Arsh Anwar <Lucifer78908@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -80,6 +80,11 @@
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
 #include <linux/cpufreq_times.h>
+<<<<<<< HEAD
+=======
+#include <linux/cpu_boost.h>
+#include <linux/scs.h>
+>>>>>>> 7597cb47c780... ANDROID: add support for clang Shadow Call Stack (SCS)
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -360,6 +365,13 @@ void free_task(struct task_struct *tsk)
 	 */
 	WARN_ON_ONCE(atomic_read(&tsk->stack_refcount) != 0);
 #endif
+<<<<<<< HEAD
+=======
+#ifdef CONFIG_CPU_FREQ_STAT
+	scs_release(tsk);
+	cpufreq_task_times_exit(tsk);
+#endif
+>>>>>>> 7597cb47c780... ANDROID: add support for clang Shadow Call Stack (SCS)
 	rt_mutex_debug_task_free(tsk);
 	ftrace_graph_exit_task(tsk);
 	put_seccomp_filter(tsk);
@@ -535,6 +547,7 @@ static struct task_struct *dup_task_struct(struct task_struct *orig, int node)
 	clear_user_return_notifier(tsk);
 	clear_tsk_need_resched(tsk);
 	set_task_stack_end_magic(tsk);
+	scs_task_init(tsk);
 
 #ifdef CONFIG_CC_STACKPROTECTOR
 	tsk->stack_canary = get_random_long();
@@ -1830,6 +1843,9 @@ static __latent_entropy struct task_struct *copy_process(
 	retval = copy_thread_tls(clone_flags, stack_start, stack_size, p, tls);
 	if (retval)
 		goto bad_fork_cleanup_io;
+	retval = scs_prepare(p, node);
+	if (retval)
+		goto bad_fork_cleanup_thread;
 
 	if (pid != &init_struct_pid) {
 		pid = alloc_pid(p->nsproxy->pid_ns_for_children);
