79abd345ed6cbaac4bd0a848c50cf04409232a14
Laura Abbott <labbott@redhat.com>
2018-05-23 11:43:46 -0700

    arm64: Make sure permission updates happen for pmd/pud
    commit 82034c23fcbc2389c73d97737f61fa2dd6526413 upstream.
    
    Commit 15122ee2c515 ("arm64: Enforce BBM for huge IO/VMAP mappings")
    disallowed block mappings for ioremap since that code does not honor
    break-before-make. The same APIs are also used for permission updating
    though and the extra checks prevent the permission updates from happening,
    even though this should be permitted. This results in read-only permissions
    not being fully applied. Visibly, this can occasionaly be seen as a failure
    on the built in rodata test when the test data ends up in a section or
    as an odd RW gap on the page table dump. Fix this by using
    pgattr_change_is_safe instead of p*d_present for determining if the
    change is permitted.
    
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Tested-by: Peter Robinson <pbrobinson@gmail.com>
    Reported-by: Peter Robinson <pbrobinson@gmail.com>
    Fixes: 15122ee2c515 ("arm64: Enforce BBM for huge IO/VMAP mappings")
    Signed-off-by: Laura Abbott <labbott@redhat.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Ben Hutchings <ben.hutchings@codethink.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -854,30 +854,18 @@ int pud_set_huge(pud_t *pudp, phys_addr_t phys, pgprot_t prot)
 {
 	pgprot_t sect_prot = __pgprot(PUD_TYPE_SECT |
 					pgprot_val(mk_sect_prot(prot)));
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 	pud_t new_pud = pfn_pud(__phys_to_pfn(phys), sect_prot);
 
 	/* Only allow permission changes for now */
-	if (!pgattr_change_is_safe(READ_ONCE(pud_val(*pud)),
+	if (!pgattr_change_is_safe(READ_ONCE(pud_val(*pudp)),
 				   pud_val(new_pud)))
-=======
-
-	/* ioremap_page_range doesn't honour BBM */
-	if (pud_present(READ_ONCE(*pudp)))
->>>>>>> 4f45a0a17035... arm64: Enforce BBM for huge IO/VMAP mappings
 		return 0;
 
 	BUG_ON(phys & ~PUD_MASK);
-	set_pud(pud, new_pud);
-=======
-	BUG_ON(phys & ~PUD_MASK);
-<<<<<<< HEAD
-	set_pud(pud, pfn_pud(__phys_to_pfn(phys), sect_prot));
->>>>>>> 17c413903026... arm64: don't open code page table entry creation
-=======
-	set_pud(pudp, pfn_pud(__phys_to_pfn(phys), sect_prot));
->>>>>>> 3103206a65f9... arm64: mm: Change page table pointer name in p[md]_set_huge()
+	set_pud(pudp, new_pud);
+>>>>>>> 246eee7b5748... arm64: Make sure permission updates happen for pmd/pud
 	return 1;
 }
 
@@ -885,6 +873,7 @@ int pmd_set_huge(pmd_t *pmdp, phys_addr_t phys, pgprot_t prot)
 {
 	pgprot_t sect_prot = __pgprot(PMD_TYPE_SECT |
 					pgprot_val(mk_sect_prot(prot)));
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 	pmd_t new_pmd = pfn_pmd(__phys_to_pfn(phys), sect_prot);
@@ -898,19 +887,27 @@ int pmd_set_huge(pmd_t *pmdp, phys_addr_t phys, pgprot_t prot)
 	set_pmd(pmd, new_pmd);
 =======
 =======
+=======
+	pmd_t new_pmd = pfn_pmd(__phys_to_pfn(phys), sect_prot);
+>>>>>>> 246eee7b5748... arm64: Make sure permission updates happen for pmd/pud
 
-	/* ioremap_page_range doesn't honour BBM */
-	if (pmd_present(READ_ONCE(*pmdp)))
+	/* Only allow permission changes for now */
+	if (!pgattr_change_is_safe(READ_ONCE(pmd_val(*pmdp)),
+				   pmd_val(new_pmd)))
 		return 0;
 
 >>>>>>> 4f45a0a17035... arm64: Enforce BBM for huge IO/VMAP mappings
 	BUG_ON(phys & ~PMD_MASK);
+<<<<<<< HEAD
 <<<<<<< HEAD
 	set_pmd(pmd, pfn_pmd(__phys_to_pfn(phys), sect_prot));
 >>>>>>> 17c413903026... arm64: don't open code page table entry creation
 =======
 	set_pmd(pmdp, pfn_pmd(__phys_to_pfn(phys), sect_prot));
 >>>>>>> 3103206a65f9... arm64: mm: Change page table pointer name in p[md]_set_huge()
+=======
+	set_pmd(pmdp, new_pmd);
+>>>>>>> 246eee7b5748... arm64: Make sure permission updates happen for pmd/pud
 	return 1;
 }
 
