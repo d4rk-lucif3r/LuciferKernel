52c6247c705ba91288ab8fca22543be43a243fea
joshuous <joshuous@gmail.com>
2018-06-17 18:23:39 +0200

    sched/tune: Introduce stune_boost() function
    Add a simple function to activate Dynamic Schedtune Boost and use the
    dynamic_boost value of the SchedTune CGroup.
    
    Signed-off-by: joshuous <joshuous@gmail.com>
    Signed-off-by: Arsh Anwar <Lucifer78908@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3980,36 +3980,9 @@ void cpufreq_remove_update_util_hook(int cpu);
 extern DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-int do_stune_boost(char *st_name, int boost, int *slot);
-int do_stune_sched_boost(char *st_name, int *slot);
-int reset_stune_boost(char *st_name, int slot);
-int do_prefer_idle(char *st_name, u64 prefer_idle);
-int set_stune_boost(char *st_name, int boost, int *boost_default);
-#else /* !CONFIG_DYNAMIC_STUNE_BOOST */
-static inline int do_stune_boost(char *st_name, int boost, int *slot)
-{
-	return 0;
-}
-
-static inline int do_stune_sched_boost(char *st_name, int *slot)
-{
-	return 0;
-}
-
-static inline int reset_stune_boost(char *st_name, int slot)
-{
-	return 0;
-}
-
-static inline int do_prefer_idle(char *st_name, u64 prefer_idle)
-{
-        return 0;
-}
-
-static inline int set_stune_boost(char *st_name, int boost, int *boost_default)
-{
-	return 0;
-}
+int do_stune_boost(char *st_name, int boost);
+int reset_stune_boost(char *st_name);
+int stune_boost(char *st_name);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 extern DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
