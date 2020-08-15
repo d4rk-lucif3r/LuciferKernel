346f114de697d3d11fe6401e1066583b882a8114
d4rk-lucif3r <lucifer78908@gmail.com>
2020-08-15 16:34:26 +0530

    Fixed Compilation Errors

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -614,23 +614,14 @@ static u8 encode_bMaxPower(enum usb_device_speed speed,
 {
 	unsigned int val = CONFIG_USB_GADGET_VBUS_DRAW;
 
-
-	if (c->MaxPower)
-		val = c->MaxPower;
-	else
-		val = CONFIG_USB_GADGET_VBUS_DRAW;
-	if (!val)
-		return 0;
-	if (speed < USB_SPEED_SUPER)
-		return min(val, 500U) / 2;
-	else
-
-		/*
-		 * USB 3.x supports up to 900mA, but since 900 isn't divisible
-		 * by 8 the integral division will effectively cap to 896mA.
-		 */
-		return min(val, 900U) / 8;
-
+	switch (speed) {
+	case USB_SPEED_SUPER:
+		/* with super-speed report 900mA */
+		val = SSUSB_GADGET_VBUS_DRAW;
+		return (u8)(val / SSUSB_GADGET_VBUS_DRAW_UNITS);
+	default:
+		return DIV_ROUND_UP(val, HSUSB_GADGET_VBUS_DRAW_UNITS);
+	}
 }
 
 static int config_buf(struct usb_configuration *config,
@@ -1045,23 +1036,8 @@ static int set_config(struct usb_composite_dev *cdev,
 		}
 	}
 
-
-	/* when we return, be sure our power usage is valid */
-	power = c->MaxPower ? c->MaxPower : CONFIG_USB_GADGET_VBUS_DRAW;
-	if (gadget->speed < USB_SPEED_SUPER)
-		power = min(power, 500U);
-	else
-		power = min(power, 900U);
-
 done:
-
-	if (power <= USB_SELF_POWER_VBUS_MAX_DRAW)
-		usb_gadget_set_selfpowered(gadget);
-	else
-		usb_gadget_clear_selfpowered(gadget);
-
-	usb_gadget_vbus_draw(gadget, power);
-
+	usb_gadget_vbus_draw(gadget, USB_VBUS_DRAW(gadget->speed));
 	if (result >= 0 && cdev->delayed_status)
 		result = USB_GADGET_DELAYED_STATUS;
 	return result;
@@ -2263,10 +2239,12 @@ void composite_disconnect(struct usb_gadget *gadget)
 	 * disconnect callbacks?
 	 */
 	spin_lock_irqsave(&cdev->lock, flags);
-
-	cdev->suspended = 0;
-	if (cdev->config)
-
+	if (cdev->config) {
+		if (gadget->is_chipidea && !cdev->suspended) {
+			spin_unlock_irqrestore(&cdev->lock, flags);
+			msm_do_bam_disable_enable(CI_CTRL);
+			spin_lock_irqsave(&cdev->lock, flags);
+		}
 		reset_config(cdev);
 	}
 	if (cdev->driver->disconnect)
@@ -2549,12 +2527,8 @@ void composite_resume(struct usb_gadget *gadget)
 {
 	struct usb_composite_dev	*cdev = get_gadget_data(gadget);
 	struct usb_function		*f;
-<<<<<<< HEAD
 	int				ret;
 	unsigned long			flags;
-=======
-	unsigned			maxpower;
->>>>>>> 6596754b90c5... usb: gadget: composite: Support more than 500mA MaxPower
 
 	/* REVISIT:  should we have config level
 	 * suspend/resume callbacks?
@@ -2587,21 +2561,7 @@ void composite_resume(struct usb_gadget *gadget)
 				f->resume(f);
 		}
 
-<<<<<<< HEAD
 		usb_gadget_vbus_draw(gadget, USB_VBUS_DRAW(gadget->speed));
-=======
-		maxpower = cdev->config->MaxPower ?
-			cdev->config->MaxPower : CONFIG_USB_GADGET_VBUS_DRAW;
-		if (gadget->speed < USB_SPEED_SUPER)
-			maxpower = min(maxpower, 500U);
-		else
-			maxpower = min(maxpower, 900U);
-
-		if (maxpower > USB_SELF_POWER_VBUS_MAX_DRAW)
-			usb_gadget_clear_selfpowered(gadget);
-
-		usb_gadget_vbus_draw(gadget, maxpower);
->>>>>>> 6596754b90c5... usb: gadget: composite: Support more than 500mA MaxPower
 	}
 
 	spin_unlock_irqrestore(&cdev->lock, flags);
