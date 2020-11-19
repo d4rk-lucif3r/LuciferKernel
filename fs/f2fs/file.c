7748be531bf28c7605ddc22d3bbe647c2b3d9966
Arsh Anwar <45912425+d4rk-lucif3r@users.noreply.github.com>
2020-11-20 01:28:26 +0530

    Update file.c

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -2195,6 +2195,8 @@ static int f2fs_ioc_set_encryption_policy(struct file *filp, unsigned long arg)
 {
 	struct inode *inode = file_inode(filp);
 
+	if (!f2fs_sb_has_encrypt(F2FS_I_SB(inode)))
+		return -EOPNOTSUPP;
 
 	f2fs_update_time(F2FS_I_SB(inode), REQ_TIME);
 
@@ -2203,6 +2205,8 @@ static int f2fs_ioc_set_encryption_policy(struct file *filp, unsigned long arg)
 
 static int f2fs_ioc_get_encryption_policy(struct file *filp, unsigned long arg)
 {
+	if (!f2fs_sb_has_encrypt(F2FS_I_SB(file_inode(filp))))
+		return -EOPNOTSUPP;
 	return fscrypt_ioctl_get_policy(filp, (void __user *)arg);
 }
 
