49aa93d47464a10db11efdd169de848edd5d2a07
Arsh Anwar <Lucifer78908@gmail.com>
2020-11-23 09:31:37 +0000

    Revert "sched/tune: Track active boosts on a per-Schedtune basis"
    This reverts commit 7f20610da23c5354c4413faf96d1b873a04d541a.

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -29,6 +29,7 @@ static DEFINE_MUTEX(boost_slot_mutex);
 static DEFINE_MUTEX(stune_boost_mutex);
 static struct schedtune *getSchedtune(char *st_name);
 static int dynamic_boost(struct schedtune *st, int boost);
+static int stune_boost_count = 0;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 /* Performance Boost region (B) threshold params */
@@ -236,9 +237,6 @@ struct schedtune {
 
 	/* Dynamic boost value for tasks on that SchedTune CGroup */
 	int dynamic_boost;
-
-	/* Number of ongoing boosts for this SchedTune CGroup */
-	int boost_count;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 };
 
@@ -282,7 +280,6 @@ root_schedtune = {
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	.boost_default = 0,
 	.dynamic_boost = 0,
-	.boost_count = 0,
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 };
 
@@ -1149,7 +1146,7 @@ static int max_active_boost(struct schedtune *st)
 
 	mutex_lock(&boost_slot_mutex);
 	mutex_lock(&stune_boost_mutex);
-	++(st->boost_count);
+	++stune_boost_count;
 
 	/* Set initial value to default boost */
 	max_boost = st->boost_default;
@@ -1210,11 +1207,11 @@ int reset_stune_boost(char *st_name, int slot)
 	boost = max_active_boost(st);
 
 	mutex_lock(&stune_boost_mutex);
-	if (st->boost_count == 1)
+	if (stune_boost_count == 1)
 		ret = dynamic_boost(st, st->boost_default);
 
-	if (st->boost_count >= 1)
-		--(st->boost_count);
+	if (stune_boost_count >= 1)
+		--stune_boost_count;
 	mutex_unlock(&stune_boost_mutex);
 
 	return ret;
