8f3b0d7c3f59fba58916f4d6747193f0f3eb60ca
Arsh Anwar <Lucifer78908@gmail.com>
2021-01-03 08:14:35 +0000

    fp-boost:Fixed Typo
    Signed-off-by: Arsh Anwar <Lucifer78908@gmail.com>

--- drivers/cpufreq/fp-boost.c
+++ drivers/cpufreq/fp-boost.c
@@ -31,7 +31,7 @@
 #include <linux/input.h>
 #include <linux/display_state.h>
 #include <linux/slab.h>
-#include <linux/time.h>include/linux/display_state.h
+#include <linux/time.h>
 
 /* Available bits for boost_policy state */
 #define DRIVER_ENABLED        (1U << 0)
