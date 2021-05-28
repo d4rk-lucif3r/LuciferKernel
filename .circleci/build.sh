12612c3a81da4d81fe3b1676db352f32d828283f
Arsh Anwar <Lucifer78908@gmail.com>
2021-05-28 09:18:39 +0000

    updated circleci
    Signed-off-by: Arsh Anwar <Lucifer78908@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Cloning dependencies"
-git clone --depth=1  https://github.com/d4rk-lucif3r/LuciferKernel.git -b nonOC-test
+git clone --depth=1  https://github.com/d4rk-lucif3r/LuciferKernel.git -b OC-test
 cd LuciferKernel
 git clone --depth=1 -b master https://github.com/kdrag0n/proton-clang clang
 git clone https://github.com/d4rk-lucif3r/Anykernel3-Tissot.git  --depth=1 AnyKernel
@@ -51,10 +51,10 @@ function compile() {
 # Zipping
 function zipping() {
     cd $REPACK_DIR || exit 1
-    zip -r9 LuciferKernel_V4_NonOC.zip *
+    zip -r9 LuciferKernel_V4_OC.zip *
     cd $SEND_DIR   || exit 1
     echo "Changing Dir to Send FIle"
-    ./telegram -t $TELEGRAM_TOKEN -c $TELEGRAM_CHAT -f $REPACK_DIR/LuciferKernel_V4_NonOC.zip "Zip Sent through CircleCi"
+    ./telegram -t $TELEGRAM_TOKEN -c $TELEGRAM_CHAT -f $REPACK_DIR/LuciferKernel_V4_OC.zip "Zip Sent through CircleCi"
    #curl https://bashupload.com/LuciferKernel+V3.zip --data-binary @LuciferKernel+V3.zip
 }
 compile
